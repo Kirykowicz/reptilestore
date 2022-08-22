c37030272c9279c920a9dfda92a3c480b2673fba
Zachary Farrell <zfarrell0507@gmail.com>
2022-08-22 17:30:10 -0400

    pics working!

--- src/reptiles/Turtles.js
+++ src/reptiles/Turtles.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AnimalDiv from "./AnimalDiv";
 
 export default function Turtles({ turtles }) {
   
   console.log(turtles)
-  /*const turtleList = turtles.turtles.map((turtle) => {
+  const turtleList = turtles.map((turtle) => {
     return <AnimalDiv key={turtle.id} species={turtle.species} image={turtle.image} price={turtle.price} />
   })
-*/
+
   return (
-    <div></div>
+    <div>{turtleList}</div>
   );
 }
