988885f3f9ab272d8511fdc4d617d819651394c2
Zachary Farrell <zfarrell0507@gmail.com>
2022-08-22 17:24:52 -0400

    Zach tests fail terribly

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import NavBar from "./NavBar";
 import Turtles from "./reptiles/Turtles";
 import Snakes from "./reptiles/Snakes";
@@ -6,12 +7,26 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./reptiles/Home";
 
 function App() {
+  const [turtles, setTurtles] = useState([])
+  const [snakes, setSnakes] = useState([])
+  const [lizards, setLizards] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/reptiles`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTurtles(data[0].turtles)
+        setSnakes(data[1].snakes)
+        setLizards(data[2].lizards)
+      });
+  }, []);
+
   return (
     <>
       <NavBar className="container" />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/turtles" element={<Turtles />} />
+        <Route path="/turtles" element={<Turtles turtles={turtles} />} />
         <Route path="/lizards" element={<Lizards />} />
         <Route path="/snakes" element={<Snakes />} />
       </Routes>
