283646d0c8a2082acb14afacb8c1ff676d61d5b4
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-08-22 14:45:22 -0500

    started adding a fetch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Turtles from "./reptiles/Turtles";
 import Snakes from "./reptiles/Snakes";
 import Lizards from "./reptiles/Lizards";
 import { Route, Routes } from "react-router-dom";
+import Home from "./reptiles/Home";
 
 function App() {
   return (
     <>
       <NavBar className="container" />
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route path="/turtles" element={<Turtles />} />
         <Route path="/lizards" element={<Lizards />} />
         <Route path="/snakes" element={<Snakes />} />
---
+++ src/reptiles/AnimalDiv.js
@@ -0,0 +1,7 @@
+export default function AnimalDiv({ animal }) {
+  return (
+    <div>
+      <h2>{animal.name}</h2>
+    </div>
+  );
+}
