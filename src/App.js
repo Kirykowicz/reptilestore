cb7e5fb35fd177ecc5275fdd3fef9d3d50755b2b
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-08-22 10:32:04 -0500

    basic navbar complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import NavBar from "./NavBar";
+import Turtles from "./reptiles/Turtles";
+import Snakes from "./reptiles/Snakes";
+import Lizards from "./reptiles/Lizards";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <NavBar className="container" />
+      <Routes>
+        <Route path="/turtles" element={<Turtles />} />
+        <Route path="/lizards" element={<Lizards />} />
+        <Route path="/snakes" element={<Snakes />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
