ee3ce319459bc1cb67a93ef7f930affccf29f5c6
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-08-22 11:51:57 -0500

    removed test changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
-        The Reptile Store test changes more tests
+        The Reptile Store
       </Link>
       <ul>
         <NavLink to="/turtles">Turtles</NavLink>
